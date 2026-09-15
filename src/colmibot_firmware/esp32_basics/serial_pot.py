e87371c031a325b14552a747b971a78550cd5a43
Daniel <danielsjohn2016@gmail.com>
2026-09-15 14:14:06 -0600

    Commit 4: Comprobacion de funcionamiento del ejemplo del potenciómetro con analog_serial_pub y analog_subs

---
+++ src/colmibot_firmware/esp32_basics/serial_pot.py
@@ -0,0 +1,14 @@
+import serial
+
+def main(args=None):
+    PORT = '/dev/ttyUSB0'
+    BAUDRATE = 115200
+
+    esp32 = serial.Serial(PORT, BAUDRATE, timeout=1)
+
+    while True:
+        linea = esp32.readline().decode().strip()
+
+        if linea:
+            print(f'ADC = {linea}')
+
